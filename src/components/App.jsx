caca1f34549685f159ddbd4615bd1f1950aee410
Leonardo <lespinoza@waypoint.cl>
2018-05-08 12:12:56 -0300

    Primer prueba: video no-streaming - muy grande no puede reproducirse

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,9 +8,21 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<div>Hello React</div>
+				<video id="mivideo" width="800" height="auto" autoPlay controls>
+					<source src="https://s3-us-west-1.amazonaws.com/hikvisionvideo/video/20180504_20180504124803_20180504193207_124804.mp4" type="video/mp4; codecs=h.264,cvd,mpga" />
+					<p>Este video no está soportado por el browser.</p>
+				</video><br />
 				<AddWordButtonComponent />
 				<ListaComponent />
 			</div>
 		);
 	}
 }
+
+/*
+				<video id="mivideo" width="800" height="auto" autoPlay controls>
+					<source src="https://s3-us-west-1.amazonaws.com/hikvisionvideo/video/20180504_20180504124803_20180504193207_124804.mp4" type="video/mp4; codecs=h.264,cvd,mpga" />
+					<p>Este video no está soportado por el browser.</p>
+				</video><br />
+					<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4" />
+*/
