975027eb74d9dd34f4a2f1f627ab9f4a4fb2beff
Leonardo <lespinoza@waypoint.cl>
2018-05-11 16:37:04 -0300

    Se agrega infraestructura JavascriptGoogleMaps, pero se agrega de momento una imagen estática

--- src/components/EspacioGIS.jsx
+++ src/components/EspacioGIS.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+//import LoadGoogleMapsApi from 'load-google-maps-api'
+
+
+const OPTIONS = {
+  center: { lat: -33.459229, lng: -70.645348 },
+  zoom: 16
+}
+
+const API_CONFIG = {
+  key:'AIzaSyBMD6YmhGzUJiyhacnKvIW678xxG5a2Ze4',
+  language: 'es'
+}
 
 const mapStateToProps = (state) => {
 	return {
@@ -17,11 +29,34 @@ const mapDistpatchToProps = (dispatch) => {
 }
 
 
-const EspacioGIS = ({ parametros }) => (
-	<div className="espacioGIS">
-		<div id="cameraMap" />
-	</div>
-);
+class EspacioGIS extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.mapaRef = React.createRef();
+	}
+
+/*	componentDidMount() {
+		LoadGoogleMapsApi( API_CONFIG ).then(googleMaps => {
+			this.googleMapCamara = new googleMaps.Map(this.refs.mapaRef, OPTIONS);
+		}).catch(err => {
+			console.log('No se pudo cargar el mapa', err);
+		});
+	}
+*/
+	render() {
+		return (
+		<div className="espacioGIS">
+			<div ref={this.mapaRef} id="cameraMap" style={{
+				backgroundImage: 'url("http://localhost/samplevideos/mapaWaypoint.png")',
+				width: '500px',
+				height: '620px'
+			}} />
+		</div>
+		);
+	}
+
+}
 
 EspacioGIS.propTypes = {
 	parametros: PropTypes.arrayOf(PropTypes.string)
